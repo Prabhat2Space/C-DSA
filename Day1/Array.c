a30d6eedbd9201d92023bcc7346c68bf47664da8
Prabhat2Space <165066651+Prabhat2Space@users.noreply.github.com>
2024-04-20 11:12:37 +0530

    Update Array.c

--- Day1/Array.c
+++ Day1/Array.c
@@ -1,3 +1,6 @@
+## User input to calculate the average marks and displays the list of all entered marks.
+
+```c
 #include<stdio.h>
 int main() {
 	int marks[20], i, n, sum = 0, avg = 0;
@@ -20,3 +23,25 @@ int main() {
 	}
 	return 0;
 }
+
+```
+
+**Output**
+```
+Enter the number of elements:
+5
+Enter the marks at 1: 80
+Enter the marks at 2: 75
+Enter the marks at 3: 90
+Enter the marks at 4: 85
+Enter the marks at 5: 95
+All the marks are set, let's see the average.
+The average score is 85.
+The total Marks List:
+Marks at 1: 80
+Marks at 2: 75
+Marks at 3: 90
+Marks at 4: 85
+Marks at 5: 95
+
+```
