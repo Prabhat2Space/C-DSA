2b51eaa6640e0000133a9a6ed518d9f7294dee44
Prabhat2Space <165066651+Prabhat2Space@users.noreply.github.com>
2024-04-20 11:29:02 +0530

    Create Datatypes.c

---
+++ Day1/Datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int myNum = 123;//To store whole numbers
+	float fNum = 4356.56;
+	char myChar = 'P';
+
+	//Lets print the values:
+	printf("%d\n", myNum);
+	printf("%f\n", fNum);
+	printf("%c\n", myChar);
+	return 0;
+}
